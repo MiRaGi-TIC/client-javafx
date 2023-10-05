8c9e0e160fe0214157f3e1da8fb46f1edb0c6a38
ramiromartinezum <135157345+ramiromartinezum@users.noreply.github.com>
2023-10-05 01:14:40 -0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module uy.edu.um.clientjavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens uy.edu.um.clientjavafx to javafx.fxml;
-    exports uy.edu.um.clientjavafx;
-}
